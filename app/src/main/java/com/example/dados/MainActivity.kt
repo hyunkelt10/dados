b6b5739b941ad73e837639ea9a3c7a9ec94e84be
ignacio <hyunkelt10@gmail.com>
2022-06-20 16:34:46 +0200

    dados nueva

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -8,45 +8,44 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b: ActivityMainBinding
+
+     private val dados = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6 )
+
+
+//     val s2 = Random.nextInt(1,7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
-
-
-        val s = Random.nextInt(1,7)
-        val imgresource = when (s) {
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
-        }
-
-        b.dado1.setImageResource(imgresource)
-
-        val s2 = Random.nextInt(1,7)
-        val imgresource2 = when (s2) {
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
-        }
-        b.dado2.setImageResource(imgresource2)
-
-        b.textView.text = (s+s2).toString()
-
-
-
-
-
     }
-    fun random1 (View:View) {
 
+    fun clickOnDado (view:View) {
+        val d1 = Random.nextInt(1,7)
+        val d2 = Random.nextInt(1,7)
+        b.dado1.setImageResource(dados [d1-1])
+        b.dado2.setImageResource(dados[d2-1])
+        b.textView.text= (d1+d2).toString()
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -55,6 +54,47 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
+//    fun random1 (View:View) {
+//
+//        val s = Random.nextInt(1,7)
+//        val imgresource = when (s) {
+//            1 -> R.drawable.dice1
+//            2 -> R.drawable.dice2
+//            3 -> R.drawable.dice3
+//            4 -> R.drawable.dice4
+//            5 -> R.drawable.dice5
+//            else -> R.drawable.dice6
+//        }
+//
+//        b.dado1.setImageResource(imgresource)
+//        b.textView.text = (s+s2).toString()
+//
+//
+//
+//
+//    }
+//
+//    fun ramdon2 (view:View) {
+//
+//        val imgresource2 = when (s2) {
+//            1 -> R.drawable.dice1
+//            2 -> R.drawable.dice2
+//            3 -> R.drawable.dice3
+//            4 -> R.drawable.dice4
+//            5 -> R.drawable.dice5
+//            else -> R.drawable.dice6
+//        }
+//        b.dado2.setImageResource(imgresource2)
+//    }
+//
+//
+//
+
+
+
+
+
 
 
 
